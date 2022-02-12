5013b6d30cdaf80ca6eddd80407704f297209358
Endaline <endalineihuoma7@gmail.com>
2022-02-12 18:34:27 +0100

    init

---
+++ Html - Copy/script.js
@@ -0,0 +1,26 @@
+function toggle(){
+    var header = document.getElementById("header")
+    header.classList.toggle("active")
+}
+// declaring class
+class Employee
+{
+    constructor(id,name)
+}
+
+{
+    //initializing an object
+    this.id = id;
+    this.name =name;
+}
+//declaring method
+detail()
+{
+   document.writeln(this.id+ " " +this.name +"<br>");            
+}
+//passing object to a variable
+var e1 = new Employee(223, "Endaline");
+var e2 = new Employee(333, "Daniel");
+//calling method
+e1.detail();
+e2.detail();
